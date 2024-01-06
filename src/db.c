dcb73e3610ecc9c77a5afd29031b447031a4b7bf
Tarun Bandi <tabandi2004@gmail.com>
2024-01-06 05:52:45 -0500

    Utility pointer arith functions for b tree

--- src/db.c
+++ src/db.c
@@ -80,6 +80,31 @@ const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
 const uint32_t LEAF_NODE_MAX_CELLS =
     LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
+uint32_t* leaf_node_num_cells(void* node){
+
+    return node + LEAF_NODE_NUM_CELLS_OFFSET;
+}
+
+void* leaf_node_cell(void* node, uint32_t cell_num){
+
+    return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE
+}
+
+uint32_t* leaf_node_key(void* node, uint32_t cell_num)
+{
+    return leaf_node_cell(node,cell_num);
+}
+
+void* leaf_node_value(void* node, uint32_t cell_num)
+{
+    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+}
+
+void initialize_leaf_node(void* node)
+{
+    *leaf_node_num_cells(node) = 0;
+}
+
 //Struct for pager -> represents a file
 typedef struct{
     int file_id;
