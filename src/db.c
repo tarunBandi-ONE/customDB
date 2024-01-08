9ffc95dd18294c6e540e2439b859cf97c9bea4e4
Tarun Bandi <tabandi2004@gmail.com>
2024-01-08 17:45:02 -0500

    Adding printing scripts

--- src/db.c
+++ src/db.c
@@ -444,6 +444,26 @@ void db_close(Table* table)
     free(table);
 }
 
+void print_constants()
+{
+    printf("ROW_SIZE: %d\n", ROWSIZE);
+    printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+    printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+    printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+    printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+    printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
+void print_leaf_node(void* node)
+{
+    uint32_t num_cells = *leaf_node_num_cells(node);
+    printf("leaf (size %d)\n", num_cells);
+    for (uint32_t i = 0; i < num_cells ; i++)
+    {
+        uint32_t key = *leaf_node_key(node, i);
+        printf("  - %d : %d\n", i, key);
+    }
+}
 
 ExecuteResult run_statement(Statement* s,Table* t)
 {
@@ -478,7 +498,19 @@ MetaCommandResult do_command(InputBuffer* input_buffer, Table* t) //Running a me
     close_input_buffer(input_buffer);
     db_close(t);
     exit(EXIT_SUCCESS); //0 
-  } else {
+  } else if (strcmp(input_buffer->buffer, ".constants") == 0) 
+  {
+    printf("Constants:\n");
+    print_constants();
+    return META_COMMAND_SUCCESS;
+  } else if (strcmp(input_buffer->buffer, ".btree") == 0) 
+  {
+    printf("Tree:\n");
+    print_leaf_node(get_page(t->pager, 0));
+    return META_COMMAND_SUCCESS;
+  }
+  
+  else {
     return META_COMMAND_UNRECOGNIZED_COMMAND;
   }
 }
